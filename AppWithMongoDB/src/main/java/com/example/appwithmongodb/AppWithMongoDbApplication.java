b2757dcb262fb986d8414b55ea1154c46dd570d9
dzontech <govedaricajovan99@gmail.com>
2021-08-18 09:00:01 -0700

    Created Project

---
+++ AppWithMongoDB/src/main/java/com/example/appwithmongodb/AppWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.appwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppWithMongoDbApplication.class, args);
+    }
+
+}
---
+++ AppWithMongoDB/src/test/java/com/example/appwithmongodb/AppWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
